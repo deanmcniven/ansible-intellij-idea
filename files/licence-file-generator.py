80c9e5a2784a65fe4baad6b9b4ee38e460edafb3
Dean McNiven <deanmcniven@users.noreply.github.com>
2018-10-17 15:42:20 +1100

    Configure IDEA

---
+++ files/licence-file-generator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import binascii
+import codecs
+import os
+import sys
+
+if len(sys.argv) != 3:
+    print "ERROR: Invalid number of parameters!"
+    print "Usage: ", sys.argv[0], " <license string> <output file>"
+    sys.exit(1)
+
+licensestring=sys.argv[1]
+outputfile=sys.argv[2]
+
+with open(outputfile, 'w') as f:
+    f.write(binascii.unhexlify('FFFF'))
+    f.write(licensestring.encode('utf-16-le'))
+
+os.chmod(outputfile, 0o644)
