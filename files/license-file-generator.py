1d0dacac008ccd5972a8c9e9423c85c4dcb5db57
Dean McNiven <deanmcniven@users.noreply.github.com>
2023-04-03 09:54:42 +1000

    Update python script to be Python3 compatible

--- files/license-file-generator.py
+++ files/license-file-generator.py
@@ -6,14 +6,14 @@ import os
 import sys
 
 if len(sys.argv) != 3:
-    print "ERROR: Invalid number of parameters!"
-    print "Usage: ", sys.argv[0], " <license string> <output file>"
+    print("ERROR: Invalid number of parameters!")
+    print("Usage: ", sys.argv[0], " <license string> <output file>")
     sys.exit(1)
 
 licensestring=sys.argv[1]
 outputfile=sys.argv[2]
 
-with open(outputfile, 'w') as f:
+with open(outputfile, 'wb') as f:
     f.write(binascii.unhexlify('FFFF'))
     f.write(licensestring.encode('utf-16-le'))
 
